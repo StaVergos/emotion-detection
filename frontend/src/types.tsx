cf6e4f6d6f0f7be86fbdddedf4eda9eb6bd4165e
StaVergos <stavrospvergos@gmail.com>
2025-07-03 17:46:12 +0300

    Added video types

---
+++ frontend/src/types.tsx
@@ -0,0 +1,25 @@
+export type Emotions = {
+    emotion: string
+    emotion_score: number
+    text: string
+    timestamp: [number, number]
+}
+
+export type VideoItem = {
+    id: string
+    audio_object: string
+    created_at: string
+    emotion_prompt_result: string
+    emotions: Emotions[]
+    transcript: string
+    transcript_process_status: "uploaded" | "processing" | "completed"
+    video_filename: string
+    video_object: string
+}
+
+export type VideoColumn = {
+    id: string
+    video_filename: string
+    transcript_process_status: "uploaded" | "processing" | "completed"
+    created_at: string
+}
