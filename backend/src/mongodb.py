f617928b3744ee5c721660a44f39d883446bd972
StaVergos <stavrospvergos@gmail.com>
2025-08-01 23:33:16 +0300

    Added collection for analysis

--- backend/src/mongodb.py
+++ backend/src/mongodb.py
@@ -4,8 +4,9 @@ from src.api.config import MONGODB_URI, MONGODB_DB
 client = MongoClient(MONGODB_URI)
 db = client[MONGODB_DB]
 emotion_detection_collection = db.emotion_detection
-
 emotion_detection_collection.create_index("video_filename", unique=True)
+openai_analysis_collection = db.openai_analysis
+openai_analysis_collection.create_index("video_id", unique=True)
 
 
 def check_record_exists(video_filename: str) -> bool:
@@ -16,3 +17,10 @@ def check_record_exists(video_filename: str) -> bool:
         emotion_detection_collection.count_documents({"video_filename": video_filename})
         > 0
     )
+
+
+def check_video_has_openai_analysis(video_id: str) -> bool:
+    """
+    Check if a record with the given video ID has an OpenAI analysis.
+    """
+    return openai_analysis_collection.count_documents({"video_id": video_id}) > 0
